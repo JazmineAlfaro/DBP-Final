16b9b93f2cb3a82c73b0ea691fb6be7c9a4535b1
FabioCarbajal <fabio.carbajal@ucsp.edu.pe>
2017-11-28 16:29:15 -0500

    Add files via upload

--- registro.php
+++ registro.php
@@ -4,6 +4,7 @@
     <title>Formulario</title>
     <meta charset="utf-8">
     <link type="text/css" href="cssfinal.css" rel="stylesheet" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 
 <body>
@@ -12,30 +13,29 @@
     </div>
     <div id="envoltura">
         <div id="contenedor">
- 
+		
+			<div id="head">Pagina de registro</div>
+			
             <div id="cuerpo">
  
                 <form id="form-login" action="#" method="post" >
-                    <p><label for="nombre">Nombre:</label></p>
-                        <input name="nombre" type="text" id="nombre" class="nombre" placeholder="Pon tu nombre" autofocus=""/ ></p>
+				
+					<p><label for="nickname">Nombre de Usuario:</label></p>
+                        <input name="nickname" type="text" id="nickname" class="nickname" placeholder="Ingresa tu nombre de usuario" autofocus=""/ ></p>
+                    
+					<p><label for="nombre">Nombre:</label></p>
+                        <input name="nombre" type="text" id="nombre" class="nombre" placeholder="Pon tu nombre" autofocus="" /></p>
  
                     <p><label for="apellidos">Apellidos:</label></p>
-                        <input name="apellidos" type="text" id="apellidos" class="apellidos" placeholder="Pon tus apellidos" /></p>
-						
-                    <p><label for="correo">Correo:</label></p>
-                        <input name="correo" type="text" id="correo" class="correo" placeholder="Pon tu mail" /></p>
+                        <input name="apellidos" type="text" id="apellidos" class="apellidos" placeholder="Pon tus apellidos" autofocus="" /></p>
  
                     <p><label for="pass">Password:</label></p>
-                        <input name="pass" type="password" id="pass" class="pass" placeholder="Pon tu contraseña"/ ></p>
- 
-                    <p><label for="repass">Confirmar Password:</label></p>
-                        <input name="repass" type="password" id="repass" class="repass" placeholder="COnfirma contraseña" /></p>
- 
-                    <p id="bot"><input name="submit" type="submit" id="boton" value="Registrar" class="boton"/></p>
+                        <input name="pass" type="password" id="pass" class="pass" placeholder="Pon tu contraseña" /></p>
+					
+                    <p id="bot"><input name="submit" type="submit" id="boton" value="Registrar" class="boton" /></p>
                 </form>
             </div>
- 
-            <div id="pie">Pagina de registro</div>
+            
         </div>
  
     </div>
