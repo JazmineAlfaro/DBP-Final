342a8cad36a90da8aea4163d40182aac683d013f
FabioCarbajal <fabio.carbajal@ucsp.edu.pe>
2017-11-28 14:58:08 -0500

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario</title>
+    <meta charset="utf-8">
+    <link type="text/css" href="cssfinal.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div id="registrar">
+            <a href="final.html">Regresar</a>
+    </div>
+    <div id="envoltura">
+        <div id="contenedor">
+ 
+            <div id="cuerpo">
+ 
+                <form id="form-login" action="#" method="post" >
+                    <p><label for="nombre">Nombre:</label></p>
+                        <input name="nombre" type="text" id="nombre" class="nombre" placeholder="Pon tu nombre" autofocus=""/ ></p>
+ 
+                    <p><label for="apellidos">Apellidos:</label></p>
+                        <input name="apellidos" type="text" id="apellidos" class="apellidos" placeholder="Pon tus apellidos" /></p>
+						
+                    <p><label for="correo">Correo:</label></p>
+                        <input name="correo" type="text" id="correo" class="correo" placeholder="Pon tu mail" /></p>
+ 
+                    <p><label for="pass">Password:</label></p>
+                        <input name="pass" type="password" id="pass" class="pass" placeholder="Pon tu contraseña"/ ></p>
+ 
+                    <p><label for="repass">Confirmar Password:</label></p>
+                        <input name="repass" type="password" id="repass" class="repass" placeholder="COnfirma contraseña" /></p>
+ 
+                    <p id="bot"><input name="submit" type="submit" id="boton" value="Registrar" class="boton"/></p>
+                </form>
+            </div>
+ 
+            <div id="pie">Pagina de registro</div>
+        </div>
+ 
+    </div>
+ 
+</body>
+ 
+</html>
